b4aa1c17f919c67c7c4dc28890048525adbd5ec4
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-08-18 14:12:59 +0200

    created datafile with mockdata

---
+++ WalkiesAndTalkies/src/data/locations.js
@@ -0,0 +1,44 @@
+const locations = [
+    {
+      name: "Lindholmen Science Park",
+      address: "Lindholmspiren 3-5, 417 56 Göteborg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/49/Lindholmen_Science_Park.jpg",
+      description: "Ett centrum för innovation och forskning inom IT, transport och media. Här möts företag, studenter och forskare.",
+      coordinates: { lat: 57.7065, lng: 11.9404 },
+      categories: ["historisk", "utsikt"]
+    },
+    {
+      name: "Kuggen",
+      address: "Lindholmsplatsen 1, 417 56 Göteborg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Kuggen%2C_Chalmers%2C_Gothenburg%2C_Sweden.jpg",
+      description: "En färgstark byggnad som tillhör Chalmers tekniska högskola. Känd för sin unika arkitektur och hållbara design.",
+      coordinates: { lat: 57.7061, lng: 11.9408 },
+      categories: ["historisk"]
+    },
+    {
+      name: "Lindholmsallén",
+      address: "Lindholmsallén, 417 56 Göteborg",
+      image: "https://i0.wp.com/nordicexperience.com/wp-content/uploads/2018/01/Gothenburg-Lindholmen.jpg",
+      description: "En central gata på Lindholmen som kantas av moderna byggnader och ett myllrande studentliv.",
+      coordinates: { lat: 57.7080, lng: 11.9400 },
+      categories: ["lugn", "historisk"]
+    },
+    {
+      name: "Älvrummet",
+      address: "Lindholmspiren 3, 417 56 Göteborg",
+      image: "https://goteborg.com/uploads/images/place/7087/960x640/150831_Aelvrummet_27.jpg",
+      description: "Ett utställningscenter om stadsutvecklingen i Göteborg, med modeller och information om framtidens stad.",
+      coordinates: { lat: 57.7073, lng: 11.9399 },
+      categories: ["historisk"]
+    },
+    {
+      name: "Kajpromenaden vid Lindholmspiren",
+      address: "Lindholmspiren, 417 56 Göteborg",
+      image: "https://goteborg.com/uploads/images/place/7090/960x640/lindholmspiren.jpg",
+      description: "En vacker promenad längs älven med utsikt över Göteborgs hamninlopp och centrum. Perfekt för en lugn promenad.",
+      coordinates: { lat: 57.7067, lng: 11.9378 },
+      categories: ["lugn", "utsikt"]
+    }
+  ];
+  
+  export default locations;
