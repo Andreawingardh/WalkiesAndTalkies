a9ee1762bcaf69e7dd2417c7c44c2a1c0c0e8dc0
Andrea Wingårdh <andreawingardh@gmail.com>
2025-08-18 15:39:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ActivityCard from './components/ActivityCard/ActivityCard';
 import activites from './data/activities'
 import locations from './data/locations'
 import MapsButton from './components/maps_button/MapsButton';
-import Button from './components/button/Button';
+import Button from './components/Button/Button';
 
 
 function App() {
