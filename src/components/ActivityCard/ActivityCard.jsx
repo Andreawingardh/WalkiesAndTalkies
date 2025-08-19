851a96991d9863f8e0b8a50b50647deec70b1b16
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-08-19 10:26:08 +0200

    added logic for when activities are out

--- src/components/ActivityCard/ActivityCard.jsx
+++ src/components/ActivityCard/ActivityCard.jsx
@@ -1,21 +1,31 @@
-import React, {useState}from "react";
+import React, {useState} from "react";
 import styles from "./ActivityCard.module.css";
 import Button from '../Button/Button';
 import activities from '../../data/activities';
 
 function ActivityCard({ question, ...props }) {
 const [currentActivity, setCurrentActivity] = useState("");
+const [activitiesArray, setActivitiesArray] = useState(activities);
+const [disabledButton, setDisabledButton] = useState(false)
 
   function getRandomActivity() {
-    setCurrentActivity(activities[Math.floor(Math.random()*activities.length)].question);
+    if (activitiesArray.length == 0) {
+      setDisabledButton(true)
+      return;
+    }
+  let number = Math.floor(Math.random()*activitiesArray.length);
+
+    setCurrentActivity(activitiesArray[number].question);
+    setActivitiesArray(activitiesArray.filter((_, index) => index !== number));
+
   }
 
   return (
     <div className={styles.normal} {...props}>
       <h1>Question</h1>
       <p>{question}</p>
-      <Button onClick={getRandomActivity}>Frågor</Button>
-      <p>{currentActivity}</p>
+      <Button onClick={getRandomActivity} disabled={disabledButton}>Frågor</Button>
+      <p>{!disabledButton ? currentActivity : "tyvärr slut på frågor"}</p>
     </div>
   );
 }
