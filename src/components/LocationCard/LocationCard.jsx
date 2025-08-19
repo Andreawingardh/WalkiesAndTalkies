e0d9f8d6a85d6d9524c5a03c6168242df96ff535
Andreawingardh <andreawingardh@gmail.com>
2025-08-19 13:55:42 +0200

    Created modal pop up for Location card

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./LocationCard.module.css";
 import MapsButton from "../maps_button/MapsButton";
 import Button from "../Button/Button";
+import LocationCardModal from "../LocationCardModal/LocationCardModal";
+import { useState } from "react";
 
 function LocationCard({
   name,
@@ -12,22 +14,45 @@ function LocationCard({
   address,
   ...props
 }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
   return (
+    <>
       <div className={styles.normal} {...props}>
-          <svg xmlns="http://www.w3.org/2000/svg" className={styles.background} width="402" height="493" viewBox="0 0 402 493" fill="none">
-  <path d="M11.3333 11.9098C171.833 97.9098 319.834 -39.5903 413.334 11.9098C497.933 58.5073 429.441 614.319 415.634 719.718C414.337 729.616 405.902 736.91 395.919 736.91H27.7632C18.2144 736.91 10.0923 730.608 8.31096 721.227C-13.0241 608.871 -135.89 -66.9761 11.3333 11.9098Z" fill="#E9E9E8"/>
-</svg>
-      <h1>{name}</h1>
-      <img src="../src/assets/images/Slottsberget.png" />
-      <div className={styles.descriptionWrapper}>
-        <p>{description}</p>
-        <p className={styles.modalLink}>Läs mer</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={styles.background}
+          width="402"
+          height="493"
+          viewBox="0 0 402 493"
+          fill="none"
+        >
+          <path
+            d="M11.3333 11.9098C171.833 97.9098 319.834 -39.5903 413.334 11.9098C497.933 58.5073 429.441 614.319 415.634 719.718C414.337 729.616 405.902 736.91 395.919 736.91H27.7632C18.2144 736.91 10.0923 730.608 8.31096 721.227C-13.0241 608.871 -135.89 -66.9761 11.3333 11.9098Z"
+            fill="#E9E9E8"
+          />
+        </svg>
+        <h1>{name}</h1>
+        <img src="../src/assets/images/Slottsberget.png" />
+        <div className={styles.descriptionWrapper}>
+          <p>{description}</p>
+          <button className={styles.modalLink} onClick={openModal}>
+            Läs mer
+          </button>
+        </div>
+        <div className={styles.buttonDisplay}>
+          <Button>Börja om</Button>
+          <MapsButton />
+        </div>
       </div>
-      <div className={styles.buttonDisplay}>
-        <Button>Börja om</Button>
-        <MapsButton />
-      </div>
-    </div>
+      <LocationCardModal isOpen={isModalOpen} onClose={closeModal}>
+              <h2>{ name }</h2>
+              <p>{ description }</p>
+        <Button onClick={closeModal} className={styles.modalButtonClose}>Stäng</Button>
+      </LocationCardModal>
+    </>
   );
 }
 
