3e1d9832ec689a87fb55e56f7de15e157d404dc5
Andreawingardh <andreawingardh@gmail.com>
2025-08-20 13:27:07 +0200

    Added dynamic data to Distance

--- src/components/Distance/Distance.jsx
+++ src/components/Distance/Distance.jsx
@@ -1,62 +1,72 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
-const Distance = () => {
-    const [walkingTime, setWalkingTime] = useState(null);
-    let startLocation = {};
+const Distance = ({ lat, lng }) => {
+  const [endPositionLat, setEndPositionLat] = useState(lat);
+  const [endPositionLng, setEndPositionLng] = useState(lng);
+  const [walkingTime, setWalkingTime] = useState(null);
+  let startLocationLat;
+  let startLocationLng;
 
-
-    const getUserLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        startLocation = {
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const startLocation = {
             lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-        // setUserLocation(startLocation);
-        
-    },
-    (error) => {
-        console.log("Error:", error.message);
-    }
-);
-} else {
-    console.log("Geolocation stöds inte");
-}
-};
+            lng: position.coords.longitude,
+          };
+          startLocationLat = startLocation.lat;
+          startLocationLng = startLocation.lng;
 
-const calculateWalkingTime = () => {
+          // setUserLocation(startLocation);
+          console.log("start" + startLocation.lng);
+          console.log("end" + endPositionLng);
+
+          calculateWalkingTime(
+            endPositionLat,
+            endPositionLng,
+            startLocationLat,
+            startLocationLng
+          );
+        },
+        (error) => {
+          console.log("Error:", error.message);
+        }
+      );
+    } else {
+      console.log("Geolocation stöds inte");
+    }
+    // return startLocation;
+  };
+
+  const calculateWalkingTime = (
+    endPositionLat,
+    endPositionLng,
+    startLocationLat,
+    startLocationLng
+  ) => {
     // if (!userLocation) {
     //         return;
     //     }
-        
-        const url = `https://graphhopper.com/api/1/route?point=${startLocation.lat},${startLocation.lng}&point=57.708215,11.943207&profile=foot&key=${import.meta.env.VITE_GRAPHHOPPER_API_KEY}`
-        
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const timeInMilliseconds = data.paths[0].time;
-            const timeInMinutes = Math.round(timeInMilliseconds / 1000 / 60);
-            setWalkingTime(timeInMinutes)
-            
-        });
-    }
-    console.log("det tar " + walkingTime + " MINUTER")
+    const url = `https://graphhopper.com/api/1/route?point=${startLocationLat},${startLocationLng}&point=${endPositionLat},${endPositionLng}&profile=foot&key=${
+      import.meta.env.VITE_GRAPHHOPPER_API_KEY
+    }`;
 
-return (
-<div>
-    <button onClick={getUserLocation}>
-        Hämta min position
-    </button>
-    <button onClick={calculateWalkingTime} >
-        Beräkna gångtid
-    </button>
-    {walkingTime && (
-        <p>Gångtid: {walkingTime} minuter</p>
-    )}
-</div>
-);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const timeInMilliseconds = data.paths[0].time;
+        const timeInMinutes = Math.round(timeInMilliseconds / 1000 / 60);
+        setWalkingTime(timeInMinutes);
+      });
+  };
+  console.log("det tar " + walkingTime + " MINUTER");
 
-}
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  return <div>{walkingTime && <p>Gångtid: {walkingTime} minuter</p>}</div>;
+};
 
 export default Distance;
