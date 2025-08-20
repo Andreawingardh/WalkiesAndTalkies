672b71a2d0a99f79b2e8f340e3369252a0f51124
Andreawingardh <andreawingardh@gmail.com>
2025-08-20 13:27:22 +0200

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import LocationCard from './components/LocationCard/LocationCard';
-import ActivityCard from './components/ActivityCard/ActivityCard';
-import activites from './data/activities'
-import locations from './data/locations'
-import MapsButton from './components/maps_button/MapsButton';
-import Button from './components/Button/Button';
-import CategoryBox from './components/Category/CategoryBox';
-import StartPage from './components/StartPage/StartPage';
-import Distance from './components/Distance/Distance';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import LocationCard from "./components/LocationCard/LocationCard";
+import ActivityCard from "./components/ActivityCard/ActivityCard";
+import activites from "./data/activities";
+import locations from "./data/locations";
+import MapsButton from "./components/maps_button/MapsButton";
+import Button from "./components/Button/Button";
+import CategoryBox from "./components/Category/CategoryBox";
+import StartPage from "./components/StartPage/StartPage";
+import Distance from "./components/Distance/Distance";
+import { useEffect, useState } from "react";
 
 function App() {
   const [showStartPage, setShowStartPage] = useState(true);
@@ -16,27 +16,12 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowStartPage(false);
-    }, 2000); 
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <>
-<Distance/> 
-      <CategoryBox/>
-    <StartPage/>
-       <CategoryBox/>
-      <Button>Question</Button>
-      <Button>Location</Button>
-      <LocationCard></LocationCard>
-      <ActivityCard></ActivityCard>
-      <MapsButton />   
-      </>
-  )
-      {showStartPage ? <StartPage /> : <CategoryBox />}
-    </>
-  );
+  return <>{showStartPage ? <StartPage /> : <CategoryBox />}</>;
 }
 
 export default App;
